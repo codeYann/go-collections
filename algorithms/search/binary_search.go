8ea793b68bde0f2a51cd5b54eb94707b8378d677
Yan Rodrigues <yan.rodriguescs@outlook.com>
2025-11-15 16:57:38 -0300

    binary search algorithm

---
+++ algorithms/search/binary_search.go
@@ -0,0 +1,25 @@
+package search
+
+import "cmp"
+
+// BinarySearch performs a binary search to find the index of target in a sorted array.
+// Returns -1 if target is not found. The array must be sorted in ascending order.
+func BinarySearch[T cmp.Ordered](arr []T, target T) int {
+	a := 0
+	b := len(arr) - 1
+
+	for a <= b {
+		middle := (a + b) / 2
+		if arr[middle] == target {
+			return middle
+		}
+
+		if arr[middle] > target {
+			b = middle - 1
+		} else {
+			a = middle + 1
+		}
+	}
+
+	return -1
+}
